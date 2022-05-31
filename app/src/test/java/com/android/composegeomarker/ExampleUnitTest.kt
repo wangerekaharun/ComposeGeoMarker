4f001e4639537decf1f5dd0cbc5efde96dae966d
Harun Wangereka <harunwangereka@gmail.com>
2022-05-31 16:07:40 +0300

    Project setup

--- app/src/test/java/com/android/composegeomarker/ExampleUnitTest.kt
+++ app/src/test/java/com/android/composegeomarker/ExampleUnitTest.kt
@@ -34,9 +34,8 @@
 
 package com.Ccomposegeomarker
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
