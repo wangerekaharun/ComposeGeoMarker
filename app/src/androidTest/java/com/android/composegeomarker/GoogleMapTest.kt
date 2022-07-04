a3d59dd6b7eb0a89dbab5c9e037018c5f8784067
Harun Wangereka <harunwangereka@gmail.com>
2022-07-05 01:07:51 +0300

    Testing

---
+++ app/src/androidTest/java/com/android/composegeomarker/GoogleMapTest.kt
@@ -0,0 +1,120 @@
+/*
+ *
+ *  * Copyright (c) 2022 Razeware LLC
+ *  *
+ *  * Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  * of this software and associated documentation files (the "Software"), to deal
+ *  * in the Software without restriction, including without limitation the rights
+ *  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  * copies of the Software, and to permit persons to whom the Software is
+ *  * furnished to do so, subject to the following conditions:
+ *  *
+ *  * The above copyright notice and this permission notice shall be included in
+ *  * all copies or substantial portions of the Software.
+ *  *
+ *  * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ *  * distribute, sublicense, create a derivative work, and/or sell copies of the
+ *  * Software in any work that is designed, intended, or marketed for pedagogical or
+ *  * instructional purposes related to programming, coding, application development,
+ *  * or information technology.  Permission for such use, copying, modification,
+ *  * merger, publication, distribution, sublicensing, creation of derivative works,
+ *  * or sale is expressly withheld.
+ *  *
+ *  * This project and source code may use libraries or frameworks that are
+ *  * released under various Open-Source licenses. Use of those libraries and
+ *  * frameworks are governed by their own individual licenses.
+ *  *
+ *  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  * THE SOFTWARE.
+ *
+ */
+
+package com.android.composegeomarker
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.CameraPositionState
+import com.google.maps.android.compose.GoogleMap
+import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertTrue
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+
+class GoogleMapTest {
+
+  @get:Rule
+  val composeTestRule = createComposeRule()
+
+  private lateinit var cameraPositionState: CameraPositionState
+  private val singapore = LatLng(1.34999984688566, 103.86999987065792)
+  private val cameraZoom = 16f
+
+  @Before
+  fun setup() {
+    cameraPositionState = CameraPositionState(
+        position = CameraPosition.fromLatLngZoom(
+            singapore,
+            cameraZoom
+        )
+    )
+  }
+
+  private fun loadMap() {
+    val countDownLatch = CountDownLatch(1)
+    composeTestRule.setContent {
+      GoogleMap(
+          modifier = Modifier.fillMaxSize(),
+          cameraPositionState = cameraPositionState,
+          onMapLoaded = {
+            countDownLatch.countDown()
+          }
+      )
+    }
+    val mapLoaded = countDownLatch.await(30, TimeUnit.SECONDS)
+    assertTrue("Map loaded", mapLoaded)
+  }
+
+  @Test
+  fun testCameraPosition() {
+    loadMap()
+    assertEquals(singapore, cameraPositionState.position.target)
+  }
+
+  @Test
+  fun testZoomInControl() {
+    loadMap()
+
+
+    composeTestRule.onNodeWithText("+")
+        .assertIsDisplayed()
+        .performClick()
+
+    composeTestRule.waitUntil(1000) {
+      cameraPositionState.isMoving
+    }
+    composeTestRule.waitUntil(3000) {
+      !cameraPositionState.isMoving
+    }
+    assertEquals(
+        cameraZoom + 1f,
+        cameraPositionState.position.zoom
+    )
+
+  }
+
+
+}
