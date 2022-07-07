fc1367f26cc18b21a8f4d7bf1d615c4e6ef0e878
Harun Wangereka <harunwangereka@gmail.com>
2022-07-07 03:06:42 +0300

    cleanup

--- app/src/main/java/com/android/composegeomarker/utils/MapUtils.kt
+++ app/src/main/java/com/android/composegeomarker/utils/MapUtils.kt
@@ -59,14 +59,3 @@ fun shouldShowPermissionRationale(context: Context, permission: String): Boolean
       permission
   )
 }
-
-
-fun PackageManager.getMetadataKey(
-    packageName: String,
-    keyName: String
-): String {
-  val ai = this.getApplicationInfo(packageName, PackageManager.GET_META_DATA);
-  val bundle = ai.metaData;
-  val key: String = bundle.getString(keyName)!!;
-  return key
-}
